29af94b0b8166c1dc2d7b7980398e0ad8d597bd8
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-20 20:51:52 +0900

    get ,put method

---
+++ main copy.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+names = {"kangji": {"age":29, "gender":"male"},
+        "bill": {"age":70, "gender": "male"}}
+
+class HelloWorld(Resource):
+    def get(self,name):
+        return names[name]
+    # def post(self):
+    #     return {"data": "posted"}
+    
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from urllib import request
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource,reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name",type=str, help="Name of video", required=True)
+video_put_args.add_argument("views",type=int, help="views of the video" ,required=True)
+video_put_args.add_argument("likes",type=int, help="Likes on the video" ,required=True)
+
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
     
-api.add_resource(HelloWorld, "/helloworld")
+    def put(self, video_id):
+        args= video_put_args.parse_args()
+        return {video_id: args}
 
+api.add_resource(Video,"/video/<int:video_id>")
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
